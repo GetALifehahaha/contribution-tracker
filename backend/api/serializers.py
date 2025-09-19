5f317bf0054cd7e05346db71fed0b10b261e033d
rauf-create539 <raufimar44@gmail.com>
2025-09-19 12:31:15 +0800

    Add user model, create user serializer

---
+++ backend/api/serializers.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {"write_only": True}}
+
+    def create(self, validated_data):
+        user = user.objects.create_user(**validated_data)
+        return user
