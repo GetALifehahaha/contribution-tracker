a3ec21be1c8905668e7896264685157e3c5c3ab4
GetALifehahaha <sencioahlannour@gmail.com>
2025-09-19 16:47:28 +0800

    create contributor + contribution: list and create view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ContributorSerializer, ContributionSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Contributor, Contribution
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class ContributorListCreate(generics.ListCreateAPIView):
+    serializer_class = ContributorSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Contributor.objects.filter(leader=user)
+    
+
+class ContributionListCreate(generics.ListCreateAPIView):
+    serializer_class = ContributionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Contribution.objects.filter(contributor__leader=user)
 
