987e88e29cde40f71b20ad4ee36f65bb3866e1bb
GetALifehahaha <sencioahlannour@gmail.com>
2025-09-19 13:51:33 +0800

    Fix serializer bug, view bug, backend url bug

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        models = User
+        model = User
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {"write_only": True}}
 
     def create(self, validated_data):
         print(validated_data)
         # hahaaha
-        user = user.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
