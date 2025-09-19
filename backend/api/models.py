a6916e84483dbcd4799a3a5ad2c6434e0845b3db
GetALifehahaha <sencioahlannour@gmail.com>
2025-09-19 16:11:15 +0800

    create contributor + contribution models

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,4 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# contribution, contributor
+# 
+class Contributor(models.Model):
+    first_name = models.CharField
+    last_name = models.CharField
 
+
+class Contribution(models.Model):
+    contributor = models.ForeignKey(
+        Contributor,
+        on_delete=models.CASCADE,
+        related_name="contributor"
+    )
+
+    amount = models.IntegerField(default=8)
+    date_of_contribution = models.DateField(auto_now_add=True)
+    date_paid = models.DateField()
+    is_paid = models.BooleanField(default=False)
